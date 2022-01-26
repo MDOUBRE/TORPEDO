d1d15f37a070b60aed02acc281aa7e774633960c
JulienBertrandIII <chezjuju24@orange.fr>
2022-01-26 18:04:39 +0100

    ajout des états de décalage gauche droite

--- code/src/suivi_de_ligne.c
+++ code/src/suivi_de_ligne.c
@@ -10,28 +10,28 @@
 
 // GPIOD
 //#define X P
-#define C1 X                            //TODO
-#define C2 X                            //TODO
-#define C3 X                            //TODO
-#define C4 X                            //TODO
-#define C5 X                            //TODO
-#define C6 X                            //TODO
-#define C7 X                            //TODO
-#define C8 X                            //TODO
+#define C1 0                            
+#define C2 1                            
+#define C3 2                            
+#define C4 3                            
+#define C5 4                            
+#define C6 5                           
+#define C7 6                            
+#define C8 7                            
 
 
 // GPIODA
 
-#define WAIT_PSC 1
+#define WAIT_PSC 100
 #define WAIT_DELAY (APB1_CLK / WAIT_PSC)
 #define HALF_PERIOD (WAIT_DELAY/2)
 
-#define SEUIL_BLANC	3
-#define SEUIL_NOIR	7
+#define SEUIL_BLANC	4
+#define SEUIL_NOIR	8
 
 volatile int un_sur_deux = 0;
 volatile int TIM4_triggered = 0;
-volatile enum{ D0 , R1 , R2 , L1 , L2} state=D0;
+volatile enum{ D0 , R1 , R2 , R3 , L1 , L2 , L3 , SR} state=D0;
 
 #define SEUIL_BLANC	3
 #define SEUIL_NOIR	7
@@ -99,9 +99,9 @@ void CHARGE()
 {
 	for (short int i=0; i < 8; i++)                              // pin 0 à 7
 	{
-		GPIOD_MODER = SET_BITS(GPIOD_MODER, i*2, 2, 0b01); //set GPIO A ou D
-		GPIOD_OTYPER = GPIOD_OTYPER & ~(1 << i);
-		GPIOD_PUPDR = SET_BITS(GPIOD_PUPDR, i*2, 2, 0b01);
+		GPIOA_MODER = SET_BITS(GPIOA_MODER, i*2, 2, 0b01); //set GPIO A ou D
+		GPIOA_OTYPER = GPIOA_OTYPER & ~(1 << i);
+		GPIOA_PUPDR = SET_BITS(GPIOA_PUPDR, i*2, 2, 0b01);
 	}
 }
 
@@ -110,9 +110,9 @@ void CAPTURE_START()
 {
 	for (short int i=0; i < 8; i++)                              // pin 0 à 7
 	{
-		GPIOD_MODER = SET_BITS(GPIOD_MODER, i*2, 2, 0b00); //set GPIO A ou D
-		GPIOD_OTYPER = GPIOD_OTYPER & ~(1 << i);
-		GPIOD_PUPDR = SET_BITS(GPIOD_PUPDR, i*2, 2, 0b01);
+		GPIOA_MODER = SET_BITS(GPIOA_MODER, i*2, 2, 0b00); //set GPIO A ou D
+		GPIOA_OTYPER = GPIOA_OTYPER & ~(1 << i);
+		GPIOA_PUPDR = SET_BITS(GPIOA_PUPDR, i*2, 2, 0b01);
 	}
 }
 
@@ -139,10 +139,186 @@ int main() {
 			TIM4_triggered = 0;
 			switch(state){
 			case D0 :
-				
-				for (int i=0; i < 8; i++)                              // pin 0 à 7
+			
+			   // mettre les roues à la même vitesse
+			   //accélérer les roues jusqu'à atteindre la vitesse max 
+			    
+				for (int i=0; i < 8; i++)                              
 				{
-					if((vc[i] > x) & ((GPIOD_IDR & (1 << i ))!= 0))
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
+					{
+						vc[i] = x;
+					}
+				}
+                if (x == 11)
+                {
+                    x = 0;
+                    CHARGE();
+                    if( vc[3] < SEUIL_NOIR)
+                    {
+                    	state = R1;
+                    	printf("R1\n");
+                    }
+                    if( vc[4] < SEUIL_NOIR)
+                    {
+                    	state = L1;
+                    	printf("R1\n");
+                    }		
+                }
+                else
+                {
+                	if (x == 1)
+                	{
+                		CAPTURE_START();
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
+                	}
+                	x = x + 1;
+                }
+				break;
+				
+			case R1 :
+			    //ralentir la roue gauche légèrement
+			    for (int i=0; i < 8; i++)                              
+				{
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
+					{
+						vc[i] = x;
+					}
+				}
+                if (x == 11)
+                {
+                    x = 0;
+                    CHARGE();
+                    if( vc[3] < SEUIL_BLANC)
+                    {
+                    	state = R2;
+                    	printf("R2\n");
+                    }
+                    if( vc[3] > SEUIL_NOIR)
+                    {
+                    	state = D0;
+                    	printf("D0\n");
+                    }
+                    if( vc[4] < SEUIL_NOIR)
+                    {
+                    	state = L1;
+                    	printf("L1\n");
+                    }
+                }
+                else
+                {
+                	if (x == 1)
+                	{
+                		CAPTURE_START();
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
+                	}
+                	x = x + 1;
+                }
+				break;
+			case R2 :
+			    //ralentir la roue gauche modérément
+			    //accélérer la roue droite légèrement si possible
+			    for (int i=0; i < 8; i++)                              
+				{
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
+					{
+						vc[i] = x;
+					}
+				}
+                if (x == 11)
+                {
+                    x = 0;
+                    CHARGE();
+                    if( vc[3] > SEUIL_BLANC)
+                    {
+                    	state = R1;
+                    	printf("R1\n");
+                    }
+                    if( vc[1] > SEUIL_NOIR || vc[0] > SEUIL_NOIR )
+                    {
+                    	state = R3;
+                    	printf("R3\n");
+                    }
+                }
+                else
+                {
+                	if (x == 1)
+                	{
+                		CAPTURE_START();
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
+                	}
+                	x = x + 1;
+                }
+				break;
+			case R3 :
+    			//ralentir la roue gauche fortement
+			    //accélérer la roue droite fortement si possible
+			    for (int i=0; i < 8; i++)                              
+				{
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
+					{
+						vc[i] = x;
+					}
+				}
+                if (x == 11)
+                {
+                    x = 0;
+                    CHARGE();
+                    if( vc[0] > SEUIL_BLANC && vc[1] > SEUIL_BLANC )
+                    {
+                    	state = SR;
+                    	printf("SR\n");
+                    }
+                    if( vc[2] > SEUIL_NOIR || vc[3] > SEUIL_NOIR )
+                    {
+                    	state = R2;
+                    	printf("R2\n");
+                    }
+                }
+                else
+                {
+                	if (x == 1)
+                	{
+                		CAPTURE_START();
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
+                	}
+                	x = x + 1;
+                }
+				break;
+				
+			case L1 :
+			    //ralentir la roue droite légèrement			
+                for (int i=0; i < 8; i++)                              
+				{
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
 					{
 						vc[i] = x;
 					}
@@ -153,34 +329,125 @@ int main() {
                     CHARGE();
                     if( vc[4] < SEUIL_BLANC)
                     {
-                    	print("trop à droite\n");
+                    	state = L2;
+                    	printf("L2\n");
                     }
-                    if( vc[5] < SEUIL_BLANC)
+                    if( vc[4] > SEUIL_NOIR)
                     {
-                    	print("trop à gauche\n");
-                    }		
+                    	state = D0;
+                    	printf("D0\n");
+                    }
+                    if( vc[3] < SEUIL_NOIR)
+                    {
+                    	state = R1;
+                    	printf("R1\n");
+                    }
                 }
                 else
                 {
                 	if (x == 1)
                 	{
                 		CAPTURE_START();
-                		vc =  { 12 , 12 , 12 , 12 , 12 , 12 , 12 , 12 };
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
+                	}
+                	x = x + 1;
+                }		
+				break;
+			case L2 :
+			    //ralentir la roue droite modérément
+			    //accélérer la roue gauche légèrement si possible
+			    for (int i=0; i < 8; i++)                              
+				{
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
+					{
+						vc[i] = x;
+					}
+				}
+                if (x == 11)
+                {
+                    x = 0;
+                    CHARGE();
+                    if( vc[4] > SEUIL_BLANC)
+                    {
+                    	state = L1;
+                    	printf("L1\n");
+                    }
+                    if( vc[6] > SEUIL_NOIR || vc[7] > SEUIL_NOIR )
+                    {
+                    	state = L3;
+                    	printf("L3\n");
+                    }
+                }
+                else
+                {
+                	if (x == 1)
+                	{
+                		CAPTURE_START();
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
                 	}
                 	x = x + 1;
                 }
 				break;
+			case L3 :
+			    //ralentir la roue droite fortement
+			    //accélérer la roue gauche fortement si possible
+			    for (int i=0; i < 8; i++)                              
+				{
+					if((vc[i] > x) & ((GPIOA_IDR & (1 << i ))!= 0))
+					{
+						vc[i] = x;
+					}
+				}
+                if (x == 11)
+                {
+                    x = 0;
+                    CHARGE();
+                    if( vc[7] > SEUIL_BLANC && vc[6] > SEUIL_BLANC )
+                    {
+                    	state = SR;
+                    	printf("SR\n");
+                    }
+                    if( vc[5] > SEUIL_NOIR || vc[4] > SEUIL_NOIR )
+                    {
+                    	state = L2;
+                    	printf("L2\n");
+                    }
+                }
+                else
+                {
+                	if (x == 1)
+                	{
+                		CAPTURE_START();
+                		vc[0] = 12;
+                		vc[1] = 12;
+                		vc[2] = 12;
+                		vc[3] = 12;
+                		vc[4] = 12;
+                		vc[5] = 12;
+                		vc[6] = 12;
+                		vc[7] = 12;
+                	}
+                	x = x + 1;
+                }
+				break;
+			case SR :
+			    //sortie de route
 			
-			/*case R1 :
 				break;
-			case R2 :
-				break;
-			case L1 :
-				break;
-			case L2 :
-				break;
-			case E5 :
-				break;*/
 		}
 	}__asm("nop");
 
